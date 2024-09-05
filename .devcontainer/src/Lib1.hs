d3ada237a87a9bf18a85717e108bb07d5117cb24
Augustė Buklerytė <auguste.bukleryte@mif.stud.vu.lt>
2024-09-05 23:31:46 +0300

    First Homework

---
+++ .devcontainer/src/Lib1.hs
@@ -0,0 +1,9 @@
+module Lib1
+    ( completions
+    ) where
+
+-- | This function returns a list of words
+-- to be autocompleted in your program's repl.
+completions :: [String]
+completions = ["Art store", "Writing utensils", "Pencils", "Pens", "Brushes", "Books", "Mystery", "Fiction", "Poetry", "Art supplies", "Ballpoint", "Fountain", "Poetry", "Canvases", "Paint", "Notebooks", "Oil", "Watercolors", "Acrylics"]
+
